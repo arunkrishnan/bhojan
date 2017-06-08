dfb52c69e381e9c7ce3893fbeda65554c43aab0f
Arunkrishnan <hereisarunkrishnan@gmail.com>
2017-06-08 20:18:32 +0530

    Adds update and delete mutations in bhojanuser
    Also set mutations to return modified node instead of success flag

--- bhojanusers/schema.py
+++ bhojanusers/schema.py
@@ -38,21 +38,21 @@ class DeliveryExecutiveNode(DjangoObjectType):
 
 
 class NewCustomer(ClientIDMutation):
-    success = String()
+    customer = Field(CustomerNode)
     class Input:
         username = String()
         first_name = String()
         last_name = String()
         email_address = String()
-        passsword = String()
+        password = String()
         phone_extension = String()
         phoneno = String()
     @classmethod
     def mutate_and_get_payload(cls, input, context, info):
         user = User.objects.create_user(input.get('username'),
-                                        input.get('email_address'),
-                                        input.get('passsword')
-                                       )
+                                    input.get('email_address'),
+                                    input.get('password')
+                                    )
         first_name = input.get('first_name')
         last_name = input.get('last_name')
         user.first_name = first_name
@@ -65,11 +65,11 @@ class NewCustomer(ClientIDMutation):
             phoneno = input.get('phoneno')
         )
         customer.save()
-        return NewCustomer(success=True)
+        return NewCustomer(customer=customer)
 
 
 class NewCustomerAddress(ClientIDMutation):
-    success = String()
+    address = Field(CustomerAddressNode)
     class Input:
         customer_id = String()
         header = String()
@@ -84,23 +84,23 @@ class NewCustomerAddress(ClientIDMutation):
             lat_long = input.get('lat_long')
         )
         address.save()
-        return NewCustomerAddress(success=True)
+        return NewCustomerAddress(address=address)
 
 
 class NewDeliveryExecutive(ClientIDMutation):
-    success = String()
+    executive = Field(DeliveryExecutiveNode)
     class Input:
         username = String()
         first_name = String()
         last_name = String()
         email_address = String()
-        passsword = String()
+        password = String()
         employee_id = String()
     @classmethod
     def mutate_and_get_payload(cls, input, context, info):
         user = User.objects.create_user(input.get('username'),
                                         input.get('email_address'),
-                                        input.get('passsword')
+                                        input.get('password')
                                        )
         first_name = input.get('first_name')
         last_name = input.get('last_name')
@@ -113,7 +113,115 @@ class NewDeliveryExecutive(ClientIDMutation):
             employee_id = input.get('employee_id')
         )
         executive.save()
-        return NewCustomer(success=True)
+        return NewDeliveryExecutive(executive=executive)
+
+
+class UpdateUser(ClientIDMutation):
+    user = Field(UserNode)
+    class Input:
+        id = String()
+        first_name = String()
+        last_name = String()
+        password = String()
+
+    @classmethod
+    def mutate_and_get_payload(cls, input, context, info):
+        user = User.objects.get(pk=from_global_id(input.get('id'))[1])
+        if input.get('first_name'):
+            user.first_name = input.get('first_name')
+        if input.get('last_name'):
+            user.last_name = input.get('last_name')
+        if input.get('password'):
+            user.set_password(input.get('password'))
+        user.save()
+        return UpdateUser(user=user)
+
+
+class UpdateCustomer(ClientIDMutation):
+    customer = Field(CustomerNode)
+    class Input:
+        id = String()
+        phone_extension = String()
+        phoneno = String()
+
+    @classmethod
+    def mutate_and_get_payload(cls, input, context, info):
+        customer = Customer.objects.get(pk=from_global_id(input.get('id'))[1])
+        if input.get('phone_extension'):
+            customer.phone_extension = input.get('phone_extension')
+        if input.get('last_name'):
+            customer.phoneno = input.get('phoneno')
+        customer.save()
+        return UpdateCustomer(customer=customer)
+
+
+class UpdateCustomerAddress(ClientIDMutation):
+    address = Field(CustomerAddressNode)
+    class Input:
+        id = String()
+        header = String()
+        address = String()
+        lat_long = Int()
+
+    @classmethod
+    def mutate_and_get_payload(cls, input, context, info):
+        customer_address = CustomerAddress.objects.get(pk=from_global_id(input.get('id'))[1])
+        if input.get('header'):
+            customer_address.header = input.get('header')
+        if input.get('address'):
+            customer_address.address = input.get('address')
+        if input.get('lat_long'):
+            customer_address.lat_long = input.get('lat_long')
+        customer_address.save()
+        return UpdateCustomerAddress(address=address)
+
+
+class DeleteUser(ClientIDMutation):
+    user = Field(UserNode)
+    class Input:
+        id = String()
+
+    @classmethod
+    def mutate_and_get_payload(cls, input, context, info):
+        user = User.objects.get(pk=from_global_id(input.get('id'))[1])
+        user.delete()
+        return DeleteUser(user=user)
+
+
+class DeleteCustomer(ClientIDMutation):
+    customer = Field(CustomerNode)
+    class Input:
+        id = String()
+
+    @classmethod
+    def mutate_and_get_payload(cls, input, context, info):
+        customer = Customer.objects.get(pk=from_global_id(input.get('id'))[1])
+        customer.delete()
+        return DeleteCustomer(customer=customer)
+
+
+class DeleteCustomerAddress(ClientIDMutation):
+    address = Field(CustomerAddressNode)
+    class Input:
+        id = String()
+
+    @classmethod
+    def mutate_and_get_payload(cls, input, context, info):
+        address = CustomerAddress.objects.get(pk=from_global_id(input.get('id'))[1])
+        address.delete()
+        return DeleteCustomerAddress(address=address)
+
+
+class DeleteDeliveryExecutive(ClientIDMutation):
+    executive = Field(DeliveryExecutiveNode)
+    class Input:
+        id = String()
+
+    @classmethod
+    def mutate_and_get_payload(cls, input, context, info):
+        executive = DeliveryExecutive.objects.get(pk=from_global_id(input.get('id'))[1])
+        executive.delete()
+        return DeleteDeliveryExecutive(executive=executive)
 
 
 class Query(AbstractType):
@@ -131,4 +239,10 @@ class Mutation(AbstractType):
     new_customer = NewCustomer.Field()
     new_customer_address = NewCustomerAddress.Field()
     new_delivery_executive = NewDeliveryExecutive.Field()
-
+    update_user = UpdateUser.Field()
+    update_customer = UpdateCustomer.Field()
+    update_customer_address = UpdateCustomerAddress.Field()
+    delete_user = DeleteUser.Field()
+    delete_customer = DeleteCustomer.Field()
+    delete_customer_address = DeleteCustomerAddress.Field()
+    delete_delivery_executive = DeleteDeliveryExecutive.Field()
